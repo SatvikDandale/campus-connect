18b3d08925221f178cd2963a638d49da75e09db1
Satvik Dandale <45892894+SatvikDandale@users.noreply.github.com>
2020-11-28 19:41:02 +0530

    Folder Move

---
+++ app/build/precache-manifest.38561fea283313124cee8e0e1fc3a0b3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc9f49a4f41892602500582ed737fa61",
+    "url": "./index.html"
+  },
+  {
+    "revision": "565fb6e8bf6ac88ee26f",
+    "url": "./static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "f2dc1e469030b450a8fb",
+    "url": "./static/css/main.16fc16f5.chunk.css"
+  },
+  {
+    "revision": "565fb6e8bf6ac88ee26f",
+    "url": "./static/js/2.4afbea4a.chunk.js"
+  },
+  {
+    "revision": "df958f16b07871c3679510165e97a61a",
+    "url": "./static/js/2.4afbea4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2dc1e469030b450a8fb",
+    "url": "./static/js/main.fefd9634.chunk.js"
+  },
+  {
+    "revision": "c361327e069463ae9554",
+    "url": "./static/js/runtime-main.a7118658.js"
+  },
+  {
+    "revision": "2d5266f6cc35918ca38ca304ff520090",
+    "url": "./static/media/abstract-dark.2d5266f6.png"
+  },
+  {
+    "revision": "f584def29c28206539067aeb2d5a039b",
+    "url": "./static/media/leftImage.f584def2.png"
+  },
+  {
+    "revision": "aaab918fa2b3c13d5e8bb13254b8ad09",
+    "url": "./static/media/profile_user@2x.aaab918f.png"
+  },
+  {
+    "revision": "105f2c051914f352c72587ecfa3bdad4",
+    "url": "./static/media/smoke-colors-abstract-qo-1536x864.105f2c05.png"
+  }
+]);
